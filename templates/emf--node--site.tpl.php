96efb99718d176570f35c75958b0f219770dd7c1
klimeto <tomas.kliment@gmail.com>
2016-12-07 17:17:38 +0100

    Added mappings to ef:observingCapability and ef:hasObservation
    observedProperty taken from field_site_params and
    field_parameters_taxonomy. For field_parameters_taxonomy the href
    defined as Sparql query to EnvThes. field_site_params has xlink:href
    parametrized:
    deimsURL/node/siteUUID/observedProperty/field_site_params_value
    Needs complex discussions about consistent URIs, SOS versus Excel data,
    etc.

--- templates/emf--node--site.tpl.php
+++ templates/emf--node--site.tpl.php
@@ -77,7 +77,7 @@ $relatedDatasetMetadataArray = json_decode($relatedDatasetMetadata);
 						//print("UUID:" .$datasetUUID ."<br>");
 						?>
 						<ef:legalBackground>
-							<base2:LegislationCitation gml:id="<?php print "Dataset_" . $datasetUUID ?>">
+							<base2:LegislationCitation gml:id="<?php print "Dataset_" . $datasetUUID . "_". uniqid()?>">
 								<base2:name>
 									<?php
 									//$legalActTextFull = $item['value'];
@@ -239,7 +239,60 @@ $relatedDatasetMetadataArray = json_decode($relatedDatasetMetadata);
 	<?php endif; ?>
 
 	<?php if (!empty($content['field_purpose'])):?>
-		<ef:purpose><?php print render($content['field_purpose']); ?></ef:purpose>
+		<!--<ef:purpose><?php print render($content['field_purpose']); ?></ef:purpose>-->
+		<ef:purpose xlink:href="http://inspire.ec.europa.eu/codelist/PurposeOfCollectionValue"/>
+	<?php endif; ?>
+	
+	<?php if (!empty($content['field_site_params']) || !empty($content['field_parameters_taxonomy'])):
+	$paramEnvthes =  strip_tags(render($content['field_parameters_taxonomy']));
+	$paramSite = render($content['field_site_params']);
+	$paramSiteArray = explode(' -- ', $paramSite);
+	$paramEnvthesArray = explode(', ', $paramEnvthes);?>
+	<?php foreach ($paramEnvthesArray as $item): ?>
+	<ef:observingCapability>
+        <ef:ObservingCapability gml:id="<?php print "ObservingCapability_" . render($content['field__site_sitecode']) . "_" . uniqid(); ?>">
+            <ef:observingTime xsi:nil="true" nilReason="Unpopulated"/>
+            
+			<!-- codeList http://inspire.ec.europa.eu/codeList/ProcessTypeValue/ with values: Process, SensorML -->
+			<ef:processType xsi:nil="true" nilReason="Unpopulated"/>
+			<!-- codeList http://inspire.ec.europa.eu/codeList/ResultNatureValue/ with values: primary, processed, simulated -->
+            <ef:resultNature xsi:nil="true" nilReason="Unpopulated"/>
+            <ef:procedure xlink:href="<?php print $deimsURL ."/node/" . $uuid . "/procedure/sensorML" ?>"/>
+			<ef:featureOfInterest xlink:href="<?php print $deimsURL ."/node/". $uuid ."/foi/foi_id" ?>"/>
+            <ef:observedProperty xlink:href="<?php 
+												$sparqlQuery = ("http://vocabs.ceh.ac.uk/evn/tbl/sparql?default-graph-uri=urn:x-evn-pub:envthes&format=text/json&query=SELECT%20%3Fresult%0AWHERE%20%7B%0A%09GRAPH%20%3Curn%3Ax-evn-pub%3Aenvthes%3E%20%7B%0A%09%09%3Fresult%20a%20%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23Concept%3E%20.%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20FILTER%20EXISTS%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%3Fresult%20%3FanyProperty%20%3FanyValue%20.%0A%20%20%20%20%20%20%20%20%20%20%20FILTER%20(isLiteral(%3FanyValue)%20%26%26%20regex(LCASE(str(%3FanyValue))%2C%20%22(%3F%3D.*". $item ."*)%22))%20.%0A%20%20%20%20%20%20%20%7D%20.%0A%09%7D%0ABIND%20(%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23prefLabel%3E(%3Fresult)%20AS%20%3Flabel)%20.%0A%7D%20ORDER%20BY%20(LCASE(%3Flabel))");
+												// Get cURL resource
+												$curl = curl_init();
+												// Set some options - we are passing in a useragent too here
+												curl_setopt_array($curl, array(
+													CURLOPT_RETURNTRANSFER => 1,
+													CURLOPT_URL => $sparqlQuery,
+													CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+												));
+												// Send the request & save response to $resp
+												$resp = curl_exec($curl);
+												// Close request to clear up some resources
+												curl_close($curl);
+												print $sparqlQuery;
+			?>"/>
+        </ef:ObservingCapability>
+    </ef:observingCapability>
+	<?php endforeach; ?>
+	<?php foreach ($paramSiteArray as $item): ?>
+	<ef:observingCapability>
+        <ef:ObservingCapability gml:id="<?php print "ObservingCapability_" . render($content['field__site_sitecode']) . "_" . uniqid(); ?>">
+            <ef:observingTime xsi:nil="true" nilReason="Unpopulated"/>
+            
+			<!-- codeList http://inspire.ec.europa.eu/codeList/ProcessTypeValue/ with values: Process, SensorML -->
+			<ef:processType xsi:nil="true" nilReason="Unpopulated"/>
+			<!-- codeList http://inspire.ec.europa.eu/codeList/ResultNatureValue/ with values: primary, processed, simulated -->
+            <ef:resultNature xsi:nil="true" nilReason="Unpopulated"/>
+            <ef:procedure xlink:href="<?php print $deimsURL ."/node/" . $uuid . "/procedure/sensorML" ?>"/>
+			<ef:featureOfInterest xlink:href="<?php print $deimsURL ."/node/". $uuid ."/foi/foi_id" ?>"/>
+            <ef:observedProperty xlink:href="<?php print $deimsURL ."/node/". $uuid ."/observedProperty/" . $item;?>"/>
+        </ef:ObservingCapability>
+    </ef:observingCapability>
+	<?php endforeach; ?>
 	<?php endif; ?>
 	
 	<?php if (!empty($content['field_parent_site_name'])):
@@ -279,6 +332,18 @@ $relatedDatasetMetadataArray = json_decode($relatedDatasetMetadata);
 			</ef:narrower>
 		<?php endforeach; ?>
 	<?php endif; ?>
+	
+	
+	<?php 
+		$sosService = render ($content['field_site_dataservi']);
+	if ($sosService === 'Sensor Web Enablement (SWE)'): ?>
+		<?php foreach ($paramSiteArray as $item): ?>
+			<ef:hasObservation xlink:href="<?php print $deimsURL . "/sos?REQUEST=GetObservation&SERVICE=SOS&VERSION=1.0.0&OFFERING=".$deimsURL."/sos/observedProperty/offeringID&OBSERVEDPROPERTY=".$deimsURL."/sos/observedProperty/".$item."&RESPONSEFORMAT=text/xml;subtype=&quot;om/1.0.0;" ?>"/>
+		<?php endforeach; ?>
+	<?php endif; ?>
+	
+	
+	
 	
 	<?php if (!empty($content['field_coordinates'])):?>
 	<ef:representativePoint>
