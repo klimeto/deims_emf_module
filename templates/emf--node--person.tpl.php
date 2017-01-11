10f8044f3e395b5e01f976747cbf96c6f98389fd
stopopol <christoph.wohner@umweltbundesamt.at>
2017-01-11 09:17:35 +0100

    Moved variable declaration to different spot
    Prevents drupal from generating warning messages

--- templates/emf--node--person.tpl.php
+++ templates/emf--node--person.tpl.php
@@ -1,11 +1,3 @@
-<?php
-$street = $content['field_address']['#object']->field_address['und'][0]['thoroughfare']; // MANDATORY FIELD WITH LABEL: Address 1
-$town = $content['field_address']['#object']->field_address['und'][0]['locality']; // MANDATORY FIELD WITH LABEL: City
-$district = $content['field_address']['#object']->field_address['und'][0]['administrative_area']; // MANDATORY FIELD WITH LABEL: Province
-$postalCode = $content['field_address']['#object']->field_address['und'][0]['postal_code']; // MANDATORY FIELD WITH LABEL: Postal code
-$country = $content['field_address']['#object']->field_address['und'][0]['country']; // OPTIONAL ???? FIELD WITH LABEL: Country
-$organizationTitle = $content['field_organization']['#object']->field_organization['und'][0]['entity']->title;
-?>
 <ef:responsibleParty>
         <base2:RelatedParty>
 			<?php print render($content['field_name']); ?>
@@ -24,7 +16,14 @@ $organizationTitle = $content['field_organization']['#object']->field_organizati
             <base2:contact>
                 <base2:Contact>
 					<?php if (!empty($content['field_address'])):?>
-					<?php //print_r ($content['field_address']['#object']->field_address['und'][0]); ?>
+						<?php
+						$street = $content['field_address']['#object']->field_address['und'][0]['thoroughfare']; // MANDATORY FIELD WITH LABEL: Address 1
+						$town = $content['field_address']['#object']->field_address['und'][0]['locality']; // MANDATORY FIELD WITH LABEL: City
+						$district = $content['field_address']['#object']->field_address['und'][0]['administrative_area']; // MANDATORY FIELD WITH LABEL: Province
+						$postalCode = $content['field_address']['#object']->field_address['und'][0]['postal_code']; // MANDATORY FIELD WITH LABEL: Postal code
+						$country = $content['field_address']['#object']->field_address['und'][0]['country']; // OPTIONAL ???? FIELD WITH LABEL: Country
+						$organizationTitle = $content['field_organization']['#object']->field_organization['und'][0]['entity']->title;
+						?>
 					<base2:contactInstructions>
                         <gco:CharacterString><?php print($street . ' ' . $town . ' ' . $district . ' ' . $postalCode . ' ' . $country); ?></gco:CharacterString>
                     </base2:contactInstructions>
